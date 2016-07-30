abec9e368cbeb6296b96e0c6a1de01fb1996eb47
drhoet <drhoet@users.noreply.github.com>
2016-07-30 14:53:08 +0200

    Marantz-side support for FM tuner

--- marantz.py
+++ marantz.py
@@ -13,6 +13,7 @@ class MarantzIp(AsyncSocketHandler):
             'ZM': self.handle_command_main_zone_power,
             'MV': self.handle_command_main_zone_volume,
             'SI': self.handle_command_main_zone_input_source_changed,
+            'TF': self.handle_command_tuner_freq,
         }
         self.main_zone_powered_on = False
 
@@ -24,6 +25,7 @@ class MarantzIp(AsyncSocketHandler):
         super().handle_connect()
         self.write_command('ZM?')
         self.write_command('MV?')
+        self.write_command('TFAN?')
 
     def command_received(self, command):
         if len(command) < 2:
@@ -83,6 +85,24 @@ class MarantzIp(AsyncSocketHandler):
         for listener in self.listeners:
             listener.on_main_zone_input_source_changed( parameter )
 
+    def set_tuner_freq(self, value):
+        if value >= 87.5 and value <= 108.0:
+            # FM!
+            self.write_command('TFAN%06d' % (int)(value * 100))
+        else:
+            self.logger.debug('Ignoring invalid FM frequency %s', value)
+            return
+
+    def handle_command_tuner_freq(self, parameter):
+        if re.match('AN\d{6}', parameter):
+            newValue = float(parameter[2:]) / 100
+        else:
+            self.logger.debug('Ignoring invalid FM frequency parameter %s', parameter)
+            return
+
+        self.logger.debug('Setting FM frequency to: %s', newValue)
+        for listener in self.listeners:
+            listener.on_tuner_freq_changed( newValue )
 
 class MarantzIpListener():
     def on_main_zone_power_changed(self, newValue):
@@ -90,4 +110,6 @@ class MarantzIpListener():
     def on_main_zone_volume_changed(self, newValue):
         pass
     def on_main_zone_input_source_changed(self, newValue):
+        pass
+    def on_tuner_freq_changed(self, newValue):
         pass
