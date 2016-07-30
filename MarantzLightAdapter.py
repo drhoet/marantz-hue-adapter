ecfd39966d42aa67e474959bd1a04cb24741d54a
drhoet <drhoet@users.noreply.github.com>
2016-07-30 14:56:51 +0200

    Using Mixins to avoid duplicate code

--- MarantzLightAdapter.py
+++ MarantzLightAdapter.py
@@ -3,7 +3,39 @@ from bridgedata import ExtendedColorLightStateListener, DimmableLightStateListen
 from marantz import MarantzIpListener
 import re
 
-class MarantzFmRadioLightAdapter(ExtendedColorLightStateListener, MarantzIpListener):
+class PowerMixin():
+
+    #########################################################
+    # methods that handle the direction light -> marantz
+    #########################################################
+
+    def on_set_power(self, oldValue, newValue):
+        self.marantzIp.set_main_zone_power( newValue )
+        if newValue:
+            self.marantzIp.set_main_zone_input_source(self.input_source)
+
+    #########################################################
+    # methods that handle the direction marantz -> light
+    #########################################################
+
+    def on_main_zone_power_changed(self, newValue):
+        if newValue == False:
+            self.light.set_on( False )
+
+    def on_main_zone_input_source_changed(self, newValue):
+        self.light.set_on( self.marantzIp.main_zone_powered_on and newValue == self.input_source)
+
+
+class VolumeMixin():
+
+    def on_set_brightness(self, oldValue, newValue):
+        self.marantzIp.set_main_zone_volume( newValue / 255 * int(self.config['Marantz']['maxvolume']) )
+
+    def on_main_zone_volume_changed(self, newValue):
+        self.light.set_brightness( int(newValue / int(self.config['Marantz']['maxvolume']) * 255) )
+
+
+class MarantzFmRadioLightAdapter(PowerMixin, VolumeMixin, ExtendedColorLightStateListener, MarantzIpListener):
 
     def __init__(self, config, light, marantzIp):
         ExtendedColorLightStateListener.__init__(self)
@@ -12,34 +44,14 @@ class MarantzFmRadioLightAdapter(ExtendedColorLightStateListener, MarantzIpListe
         self.config = config
         self.light = light
         self.marantzIp = marantzIp
+        self.input_source = 'TUNER'
         self.logger = logging.getLogger('MarantzFmRadioLightAdapter')
-
-    #########################################################
-    # methods that handle the direction light -> marantz
-    #########################################################
-    def on_set_power(self, oldValue, newValue):
-        self.marantzIp.set_main_zone_power( newValue )
-        if newValue:
-            self.marantzIp.set_main_zone_input_source('IRP')
-
-    def on_set_brightness(self, oldValue, newValue):
-        self.marantzIp.set_main_zone_volume( newValue / 255 * int(self.config['Marantz']['maxvolume']) )
-
-    #########################################################
-    # methods that handle the direction marantz -> light
-    #########################################################
-    def on_main_zone_power_changed(self, newValue):
-        if newValue == False:
-            self.light.set_on( False )
-
-    def on_main_zone_volume_changed(self, newValue):
-        self.light.set_brightness( int(newValue / int(self.config['Marantz']['maxvolume']) * 255) )
-
-    def on_main_zone_input_source_changed(self, newValue):
-        self.light.set_on( self.marantzIp.main_zone_powered_on and (newValue == 'IRADIO' or newValue == 'IRP') )
+        self.data = []
+        self.fileindex = 0
 
 
-class MarantzMediaPlayerLightAdapter(DimmableLightStateListener, MarantzIpListener):
+
+class MarantzMediaPlayerLightAdapter(PowerMixin, VolumeMixin, DimmableLightStateListener, MarantzIpListener):
     def __init__(self, config, light, marantzIp):
         DimmableLightStateListener.__init__(self)
         light.register_listener(self)
@@ -47,28 +59,5 @@ class MarantzMediaPlayerLightAdapter(DimmableLightStateListener, MarantzIpListen
         self.config = config
         self.light = light
         self.marantzIp = marantzIp
+        self.input_source = 'MPLAY'
         self.logger = logging.getLogger('MarantzMediaPlayerLightAdapter')
-
-    #########################################################
-    # methods that handle the direction light -> marantz
-    #########################################################
-    def on_main_zone_power_changed(self, newValue):
-        if newValue == False:
-            self.light.set_on( False )
-
-    def on_set_power(self, oldValue, newValue):
-        self.marantzIp.set_main_zone_power( newValue )
-        if newValue:
-            self.marantzIp.set_main_zone_input_source('MPLAY')
-
-    def on_set_brightness(self, oldValue, newValue):
-        self.marantzIp.set_main_zone_volume( newValue / 255 * int(self.config['Marantz']['maxvolume']) )
-
-    #########################################################
-    # methods that handle the direction marantz -> light
-    #########################################################
-    def on_main_zone_volume_changed(self, newValue):
-        self.light.set_brightness( int(newValue / int(self.config['Marantz']['maxvolume']) * 255) )
-
-    def on_main_zone_input_source_changed(self, newValue):
-        self.light.set_on( self.marantzIp.main_zone_powered_on and newValue == 'MPLAY' )
